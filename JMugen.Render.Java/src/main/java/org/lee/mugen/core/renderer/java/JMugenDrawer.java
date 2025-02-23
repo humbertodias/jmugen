c57d0a01b00b61c6ba46e3f533f1cc9b7a75e173
Humberto Dias <hldtux@gmail.com>
2025-02-23 13:08:19 -0300

    add PalfxFilter

--- JMugen.Render.Java/src/main/java/org/lee/mugen/core/renderer/java/JMugenDrawer.java
+++ JMugen.Render.Java/src/main/java/org/lee/mugen/core/renderer/java/JMugenDrawer.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import org.lee.mugen.imageIO.PCXLoader;
 import org.lee.mugen.imageIO.RawPCXImage;
 import org.lee.mugen.imageIO.PCXLoader.PCXHeader;
+import org.lee.mugen.object.Rectangle;
 import org.lee.mugen.renderer.AngleDrawProperties;
 import org.lee.mugen.renderer.DrawProperties;
 import org.lee.mugen.renderer.GameWindow;
@@ -20,137 +21,148 @@ import org.lee.mugen.renderer.MugenDrawer;
 import org.lee.mugen.renderer.Trans;
 
 import composite.BlendComposite;
-import org.lee.mugen.util.Logger;
 
 public class JMugenDrawer extends MugenDrawer {
-
-	private float alpha = 1.0f;
-	private float xScale = 1.0f;
-	private float yScale = 1.0f;
-	private Color c = Color.BLACK;
-	private JGameWindow gameWindow;
 
 	private void processRotationProperties(AngleDrawProperties dp) {
 		if (dp != null) {
 			JGameWindow window = (JGameWindow) getInstanceOfGameWindow();
-			Graphics2D g = window.getDrawGraphics();
+			Graphics2D g = (Graphics2D) window.getDrawGraphics();
 			AffineTransform rot = AffineTransform.getRotateInstance(dp.getAngleset(), dp.getXAnchor(), dp.getYAnchor());
 			g.setTransform(rot);
 		}
+
 	}
+
 
 	@Override
 	public void draw(DrawProperties dp) {
 		JGameWindow window = (JGameWindow) getInstanceOfGameWindow();
-		Graphics2D g = window.getDrawGraphics();
+		Graphics2D g = (Graphics2D) window.getDrawGraphics();
 		g.scale(xScale, yScale);
 		BufferedImage img = (BufferedImage) dp.getIc().getImg();
+		g.setClip((int)dp.getXLeftDst(), (int)dp.getYTopDst(), img.getWidth(), img.getHeight());
 
-		if (img == null) {
-			return; // If the image is null, there is no point in continuing the drawing.
+		Composite composite = null;
+		if (dp.getTrans() == Trans.ADD) {
+			if(alpha == 1)
+				composite = BlendComposite.Add;
+			else
+				composite = BlendComposite.Add.derive(alpha);
+
+		} else if (dp.getTrans() == Trans.ADD1) {
+			if(alpha == 1)
+				composite = BlendComposite.Add;
+			else
+				composite = BlendComposite.Add.derive(alpha);
+
+		} else if (dp.getTrans() == Trans.SUB) {
+			if(alpha == 1)
+				composite = BlendComposite.Subtract.derive(0.5f);
+			else
+				composite = BlendComposite.Subtract.derive(0.5f*alpha);
+
 		}
-
-		g.setClip((int) dp.getXLeftDst(), (int) dp.getYTopDst(), img.getWidth(), img.getHeight());
-
-		Composite composite = getComposite(dp.getTrans());
 		if (composite != null) {
 			g.setComposite(composite);
 		} else if (alpha != 1) {
-			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC, alpha));
+			composite = AlphaComposite.getInstance(AlphaComposite.SRC, alpha);
+			g.setComposite(composite);
 		}
+
+
 
 		processRotationProperties(dp.getAngleDrawProperties());
-
+		PalfxFilter palfxFilter = new PalfxFilter();
 		if (null != dp.getPalfx()) {
-//			palfxFilter.setFxSub(dp.getPalfx());
-//			img = palfxFilter.filter(img, null);
+			palfxFilter.setFxSub(dp.getPalfx());
+			img = palfxFilter.filter(img, null);
 		}
 
-		float localXScale = 1f;
-		float localYScale = 1f;
+		float xScale = 1f;
+		float yScale = 1f;
 
 		if (dp.getAngleDrawProperties() != null) {
-			localXScale = dp.getAngleDrawProperties().getXScale();
-			localYScale = dp.getAngleDrawProperties().getYScale();
+			xScale = dp.getAngleDrawProperties().getXScale();
+			yScale = dp.getAngleDrawProperties().getYScale();
 		}
+		g.setClip((int)dp.getXLeftDst(), (int)dp.getYTopDst(), img.getWidth(), img.getHeight());
+		g.drawImage(
+				img,
+				(int)dp.getXLeftDst(),
+				(int)dp.getYTopDst(),
+				(int) ((dp.getXRightDst() - dp.getXLeftDst()) * dp.getXScaleFactor() * xScale + dp.getXLeftDst()),
+				(int) ((dp.getYBottomDst() - dp.getYTopDst()) * dp.getYScaleFactor() * yScale + dp.getYTopDst()),
+				(int)dp.getXLeftSrc(),
+				(int)dp.getYTopSrc(),
+				(int)dp.getXRightSrc(),
+				(int)dp.getYBottomSrc(), null);
 
-		g.drawImage(img,
-				(int) dp.getXLeftDst(),
-				(int) dp.getYTopDst(),
-				(int) ((dp.getXRightDst() - dp.getXLeftDst()) * dp.getXScaleFactor() * localXScale + dp.getXLeftDst()),
-				(int) ((dp.getYBottomDst() - dp.getYTopDst()) * dp.getYScaleFactor() * localYScale + dp.getYTopDst()),
-				(int) dp.getXLeftSrc(),
-				(int) dp.getYTopSrc(),
-				(int) dp.getXRightSrc(),
-				(int) dp.getYBottomSrc(), null);
+
 	}
-
-	private Composite getComposite(Trans trans) {
-		if (trans == null) return null;
-
-		switch (trans) {
-			case ADD:
-			case ADD1:
-				return (alpha == 1) ? BlendComposite.Add : BlendComposite.Add.derive(alpha);
-			case SUB:
-				return BlendComposite.Subtract.derive(0.5f * alpha);
-			default:
-				return null;
-		}
-	}
-
 	@Override
 	public void drawLine(int x1, int y1, int x2, int y2) {
 		JGameWindow window = (JGameWindow) getInstanceOfGameWindow();
-		Graphics2D g = window.getDrawGraphics();
+		Graphics2D g = (Graphics2D) window.getDrawGraphics();
 		g.setColor(c);
 		g.drawLine(x1, y1, x2, y2);
+
 	}
 
 	@Override
 	public void drawRect(float x, float y, float width, float height) {
 		JGameWindow window = (JGameWindow) getInstanceOfGameWindow();
-		Graphics2D g = window.getDrawGraphics();
+		Graphics2D g = (Graphics2D) window.getDrawGraphics();
 		g.setColor(c);
-		g.drawRect((int) x, (int) y, (int) width, (int) height);
+		g.drawRect((int)x, (int)y, (int)width, (int)height);
 	}
 
 	@Override
 	public void fillRect(float x, float y, float width, float height) {
 		JGameWindow window = (JGameWindow) getInstanceOfGameWindow();
-		Graphics2D g = window.getDrawGraphics();
+		Graphics2D g = (Graphics2D) window.getDrawGraphics();
 		g.setColor(c);
-		g.fillRect((int) x, (int) y, (int) width, (int) height);
-	}
+		g.fillRect((int)x, (int)y, (int)width, (int)height);
 
+	}
+	private Color c = Color.BLACK;
 	@Override
 	public void setColor(float r, float g, float b, float a) {
-		c = new Color((int) r, (int) g, (int) b, (int) a);
+		JGameWindow window = (JGameWindow) getInstanceOfGameWindow();
+		Graphics2D g2D = (Graphics2D) window.getDrawGraphics();
+
+		g2D.setColor(new Color((int)r, (int)g, (int)b, (int)a));
+		c = new Color((int)r, (int)g, (int)b, (int)a);
 	}
 
 	@Override
 	public void setColor(float r, float g, float b) {
-		c = new Color((int) r, (int) g, (int) b);
+		JGameWindow window = (JGameWindow) getInstanceOfGameWindow();
+		Graphics2D g2D = (Graphics2D) window.getDrawGraphics();
+
+		g2D.setColor(new Color((int)r, (int)g, (int)b));
+
 	}
 
+	private float xScale = 1;
+	private float yScale = 1;
 	@Override
 	public void scale(float x, float y) {
 		xScale *= x;
 		yScale *= y;
+
 	}
 
 	@Override
 	public ImageContainer getImageContainer(Object imageData) {
 		RawPCXImage pcx = (RawPCXImage) imageData;
-		PCXHeader header;
+		PCXHeader header = null;
 
 		try {
 			header = new PCXHeader(pcx.getData());
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
-
 		int width = header.getWidth();
 		int height = header.getHeight();
 
@@ -160,76 +172,66 @@ public class JMugenDrawer extends MugenDrawer {
 				if (img instanceof BufferedImage) {
 					return img;
 				}
-
+				RawPCXImage pcx = (RawPCXImage) img;
+				BufferedImage image = null;
 				try {
-					img = PCXLoader.loadImageColorIndexed(new ByteArrayInputStream(pcx.getData()), pcx.getPalette(), false, true);
+					image = (BufferedImage) PCXLoader.loadImageColorIndexed(new ByteArrayInputStream(
+							pcx.getData()), pcx.getPalette(), false, true);
 				} catch (IOException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				return img;
+				return img = image;
 			}
 		};
+
 	}
 
+
+	private JGameWindow gameWindow;
 	@Override
 	public GameWindow getInstanceOfGameWindow() {
-		if (gameWindow == null) {
+		if (gameWindow == null)
 			gameWindow = new JGameWindow();
-		}
 		return gameWindow;
 	}
+
 
 	@Override
 	public ImageContainer getImageContainer(Object imageData, int colors) {
 		RawPCXImage pcx = (RawPCXImage) imageData;
 		try {
-			BufferedImage image = PCXLoader.loadImageColorIndexed(new ByteArrayInputStream(pcx.getData()), pcx.getPalette(), false, true, colors);
+//			BufferedImage image = (BufferedImage) PCXLoader.loadImage(new ByteArrayInputStream(
+//					pcx.getData()), pcx.getPalette(), false, true);
+
+			BufferedImage image = (BufferedImage) PCXLoader.loadImageColorIndexed(new ByteArrayInputStream(
+					pcx.getData()), pcx.getPalette(), false, true, colors);
+
 			return new ImageContainer(image, image.getWidth(), image.getHeight());
 		} catch (IOException e) {
-			throw new IllegalArgumentException("Failed to load image", e);
+			throw new IllegalArgumentException();
 		}
+
 	}
+
 
 	@Override
-	public void setClip(org.lee.mugen.object.Rectangle clipRect) {
-		JGameWindow window = (JGameWindow) getInstanceOfGameWindow();
-		Graphics2D g2d = window.getDrawGraphics();
+	public void setClip(Rectangle r) {
+		// TODO Auto-generated method stub
 
-		if (clipRect != null) {
-			Logger.log("Setting clip area to: " + clipRect);
-
-			// Doubling the coordinates as per original logic
-			int left = clipRect.getX1() * 2;
-			int top = clipRect.getY1() * 2;
-			int right = clipRect.getX2() * 2;
-			int bottom = clipRect.getY2() * 2;
-
-			// Setting the clipping area
-			g2d.setClip(left, top, right - left, bottom - top);
-
-			// Applying scaling to double the drawing size
-			AffineTransform transform = new AffineTransform();
-			transform.scale(2.0, 2.0);
-			g2d.setTransform(transform);
-		} else {
-			// No clipping, draw on the entire screen
-			g2d.setClip(0, 0, window.getWidth(), window.getHeight());
-
-			// Applying default scaling based on window dimensions
-			AffineTransform transform = new AffineTransform();
-			transform.scale((double) window.getWidth() / 320, (double) window.getHeight() / 240);
-			g2d.setTransform(transform);
-		}
 	}
 
-
+	float alpha = 1;
 	@Override
 	public float getAlpha() {
 		return alpha;
+
 	}
+
 
 	@Override
 	public void setAlpha(float a) {
 		this.alpha = a;
 	}
+
 }
